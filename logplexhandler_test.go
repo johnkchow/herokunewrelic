826e3e1153deda7f6bbf7f9ef3f0ecb9b0ab09c7
John Chow <jchow@reflektive.com>
2018-06-21 08:35:04 -0700

    Include router log messages in test

--- logplexhandler_test.go
+++ logplexhandler_test.go
@@ -13,6 +13,7 @@ import (
 var frames = [][]byte{
 	[]byte("195 <40>1 2012-11-30T06:45:29+00:00 host heroku web.4 - source=web.4 dyno=heroku.18186867.69773a68-35f0-4cc3-aa48-e956a04c2c8b sample#load_avg_1m=0.27 sample#load_avg_5m=0.22 sample#load_avg_15m=0.19"),
 	[]byte("77 <40>3 2012-11-30T06:45:29+00:00 host app web.4 - Some application log message"),
+	[]byte(`245 <40>3 2012-11-30T06:45:29+00:00 host heroku router - at=info method=GET path="/" host=some-app.herokuapp.com request_id=78b767f8-fa4d-42a9-8b1d-b71bc22a8934 fwd="127.0.0.1" dyno=web.4 connect=1ms service=28ms status=200 bytes=1084 protocol=https`),
 	[]byte("332 <40>2 2012-11-30T06:45:29+00:00 host heroku web.4 - source=web.4 dyno=heroku.18186867.69773a68-35f0-4cc3-aa48-e956a04c2c8b sample#memory_total=7020.55MB sample#memory_rss=7013.48MB sample#memory_cache=7.08MB sample#memory_swap=0.00MB sample#memory_pgpgin=1949515pages sample#memory_pgpgout=779250pages sample#memory_quota=14336.00MB"),
 	[]byte("551 <40>3 2012-11-30T06:45:29+00:00 host app heroku-postgres - source=HEROKU_POSTGRESQL_ONYX sample#current_transaction=153674642 sample#db_size=53666224664bytes sample#tables=105 sample#active-connections=20 sample#waiting-connections=0 sample#index-cache-hit-rate=0.99832 sample#table-cache-hit-rate=0.9657 sample#load-avg-1m=0.01 sample#load-avg-5m=0.045 sample#load-avg-15m=0.025 sample#read-iops=0.58264 sample#write-iops=3.8988 sample#memory-total=8173656kB sample#memory-free=1124196kB sample#memory-cached=5935596kB sample#memory-postgres=111996kB"),
 }
@@ -71,7 +72,7 @@ func init() {
 func buildRequest() *http.Request {
 	req, _ := http.NewRequest("GET", "/", bytes.NewReader(logplexBody))
 
-	req.Header.Add("Logplex-Msg-Count", "4")
+	req.Header.Add("Logplex-Msg-Count", "5")
 	req.Header.Add("Logplex-Msg-Id", "someid")
 	req.Header.Add("Logplex-Drain-Token", "draintoken")
 	req.Header.Add("User-Agent", "Logplex/v72")
